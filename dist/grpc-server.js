311518c6eab5e2f6d0805a55fb36e9915349a746
Aleksei <alexey.sv.97@gmail.com>
2025-02-13 14:44:34 -0500

    2nd commit

---
+++ dist/grpc-server.js
@@ -0,0 +1,70 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const grpc = __importStar(require("@grpc/grpc-js"));
+const protoLoader = __importStar(require("@grpc/proto-loader"));
+const html_pdf_1 = __importDefault(require("html-pdf"));
+const path = __importStar(require("path"));
+const PROTO_PATH = path.join(__dirname, 'proto', 'HTML2PDFConverter.proto');
+// Load the .proto file
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+});
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+const html2pdfProto = protoDescriptor.HTML2PDFConverter;
+const server = new grpc.Server();
+server.addService(html2pdfProto.service, {
+    ConvertHTMLToPDF: (call, callback) => {
+        const htmlContent = Buffer.from(call.request.data).toString('utf-8');
+        html_pdf_1.default.create(htmlContent).toBuffer((err, buffer) => {
+            if (err) {
+                return callback(err, null);
+            }
+            callback(null, { data: buffer });
+        });
+    },
+});
+// Start the gRPC server
+server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    console.log('✅ gRPC Server running on port 50051');
+    server.start();
+});
